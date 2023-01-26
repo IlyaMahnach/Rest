2ad6bd9aff16e12feae18f70dcf41637e3eab2b0
IlyaMahnach <ilya_mahnach@mail.ru>
2023-01-26 14:01:37 +0700

    change

--- src/main/java/SightsController.java
+++ src/main/java/SightsController.java
@@ -7,6 +7,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 public class SightsController {
+
+    private SightsRepository sightsRepository;
+
+    @Autowired
+    public SightsController(SightsRepository sightsRepository) {
+        this.sightsRepository = sightsRepository;
+    }
+
+    @GetMapping("/sight/all")
+    public List<Sight> allCities() {
+        return sightsRepository.findAll();
+    }
+
+    @DeleteMapping("/sight/1")
+    public void deleteCities(@PathVariable(name = "1") Sight sight) {
+        sightsRepository.delete(sight);
+    }
 }
